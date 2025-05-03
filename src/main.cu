ee99fe91eaa96e8ddaa0899558556e08389cd306
Ankit Bando <ankitbando@gmail.com>
2025-05-02 20:49:40 -0400

    fixed

--- src/main.cu
+++ src/main.cu
@@ -3,6 +3,18 @@
 
 #define BLOCK_SIZE 256
 
+#define CHECK_CUDA_ERROR(val) check((val), #val, __FILE__, __LINE__)
+
+template <typename T>
+void check(T err, const char* const func, const char* file, const int line) {
+    if (err != cudaSuccess) {
+        fprintf(stderr, "CUDA error at %s:%d code=%d(%s) \"%s\" \n", file, 
+            line, static_cast<unsigned int>(err), cudaGetErrorString(err), 
+            func);
+        exit(EXIT_FAILURE);
+    }
+}
+
 __global__ void generate_frame(unsigned int *buffer, int width, int height) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     int total_pixels = width * height;
@@ -23,44 +35,34 @@ __global__ void generate_frame(unsigned int *buffer, int width, int height) {
 }
 
 int main() {
-    int image_width = 256;
-    int image_height = 256;
+    int image_width = 4096;
+    int image_height = 2160;
     int total_pixels = image_width * image_height;
     int buffer_size = total_pixels * 3;
+    cudaEvent_t start, stop;
+    cudaStream_t stream1;
 
     int total_blocks = (total_pixels + BLOCK_SIZE - 1) / BLOCK_SIZE; // (256 * 256 + 128 - 1) / 256 = 256 blocks
 
-    unsigned int *frame_buffer = new unsigned int[buffer_size]; // The frame buffer.
+    unsigned int *frame_buffer; // The Host's frame buffer.
 
-    unsigned int *device_frame_buffer;
+    unsigned int *device_frame_buffer; // Device's frame buffer.
+
+    CHECK_CUDA_ERROR(cudaEventCreate(&start));
+    CHECK_CUDA_ERROR(cudaEventCreate(&stop));
+
+    CHECK_CUDA_ERROR(cudaStreamCreate(&stream1));
     
-    cudaMalloc(&device_frame_buffer, sizeof(int) * buffer_size);
+    CHECK_CUDA_ERROR(cudaEventRecord(start, stream1));
+    
+    CHECK_CUDA_ERROR(cudaMallocHost(&frame_buffer, sizeof(int) * buffer_size));
+    CHECK_CUDA_ERROR(cudaMallocAsync(&device_frame_buffer, sizeof(int) * buffer_size, stream1));
 
-    // After cudaMalloc
-    cudaError_t error = cudaGetLastError();
-    if(error != cudaSuccess) {
-        std::cerr << "Error after cudaMalloc: " << cudaGetErrorString(error) << std::endl;
-        return -1;
-    }
+    generate_frame<<<total_blocks, BLOCK_SIZE, 0, stream1>>>(device_frame_buffer, image_width, image_height);
 
-    generate_frame<<<total_blocks, BLOCK_SIZE>>>(device_frame_buffer, image_width, image_height);
+    CHECK_CUDA_ERROR(cudaStreamSynchronize(stream1));
 
-    // After kernel launch
-    error = cudaGetLastError();
-    if(error != cudaSuccess) {
-        std::cerr << "Error after kernel launch: " << cudaGetErrorString(error) << std::endl;
-        return -1;
-    }
-    cudaDeviceSynchronize();
-
-    cudaMemcpy(frame_buffer, device_frame_buffer, sizeof(int) * buffer_size, cudaMemcpyDeviceToHost);
-
-    // After cudaMemcpy
-    error = cudaGetLastError();
-    if(error != cudaSuccess) {
-        std::cerr << "Error after cudaMemcpy: " << cudaGetErrorString(error) << std::endl;
-        return -1;
-    }
+    CHECK_CUDA_ERROR(cudaMemcpyAsync(frame_buffer, device_frame_buffer, sizeof(int) * buffer_size, cudaMemcpyDeviceToHost, stream1));
 
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
@@ -71,8 +73,20 @@ int main() {
         }
     }
 
-    delete[] frame_buffer;
-    cudaFree(device_frame_buffer);
+    CHECK_CUDA_ERROR(cudaEventRecord(stop, stream1));
+
+    CHECK_CUDA_ERROR(cudaEventSynchronize(stop));
+    float milliseconds = 0;
+
+    CHECK_CUDA_ERROR(cudaEventElapsedTime(&milliseconds, start, stop));
+
+    CHECK_CUDA_ERROR(cudaFreeHost(frame_buffer));
+    CHECK_CUDA_ERROR(cudaFree(device_frame_buffer));
+    CHECK_CUDA_ERROR(cudaStreamDestroy(stream1));
+    CHECK_CUDA_ERROR(cudaEventDestroy(start));
+    CHECK_CUDA_ERROR(cudaEventDestroy(stop));
+
+    std::cerr << "Execution time: " << milliseconds << " milliseconds" << std::endl;
 
     return 0;
 }
